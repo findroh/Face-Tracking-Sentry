420b801294ca054fc8fded1375d6af183dee32c0
findroh <87558874+findroh@users.noreply.github.com>
2025-12-01 23:23:36 -0600

    ServoControl.ino
    This is a work in progress. Right now, the PID control factors are likely too large or the way I try to correct for error is flawed which leads to the motors overcorrecting and making the motors move far from the subject. I will upload the correct version when I sort these issues out.

---
+++ ServoControl.ino
@@ -0,0 +1,65 @@
+#include <ESP32Servo.h>
+#include <math.h>
+Servo servoX;
+Servo servoY;
+int servoPinX = 6;
+int servoPinY = 5;
+int currentAngle_X = 90;
+int currentAngle_Y = 90;
+double Kp = 0.01;
+double Ki = 0.0001;
+double Kd = 0.001;
+double dt = 0;
+double previousTime = 0;
+double integral = 0;
+double previousError = 0;
+void setup() {
+  // put your setup code here, to run once:
+
+ Serial.begin(115200);
+ Serial.println("ESP32 Ready to Receive");
+ servoX.attach(servoPinX);
+ servoY.attach(servoPinY);
+ servoX.write(currentAngle_X);
+ servoY.write(currentAngle_Y);
+  
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+   if(Serial.available()){
+
+    double currentTime = millis();
+    dt = (currentTime - previousTime) / 1000;
+    String receivedString = Serial.readStringUntil('\n');
+    String receivedString2 = Serial.readStringUntil('\n');
+    Serial.print("received integers");
+    int receivedInt = receivedString.toInt();
+    int receivedInt2 = receivedString2.toInt();
+    Serial.println("Received Integers: ");
+    Serial.println(receivedInt);
+    Serial.println(receivedInt2);
+    int x_error = receivedInt - 319;
+    int y_error = receivedInt2 - 239;
+    int X_Angle = currentAngle_X - pid(x_error);
+    int Y_Angle = currentAngle_Y - pid(y_error);
+    servoX.write(X_Angle);
+    servoY.write(Y_Angle);
+    
+    }
+
+    
+}
+
+int pid(int error){
+  int proportional = error;
+  integral = integral + (error * dt);
+  double derivative = (error - previousError) / dt;
+  double output = (Kp * proportional) * (Ki * integral) + (Kd * derivative);
+  int myOutput = (int)ceil(output);
+  previousError = error;
+
+  return myOutput;
+  }
